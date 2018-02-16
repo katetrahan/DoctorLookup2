82eb91a6f2edf4fa0288ecd624c2ae095001288f
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-16 15:48:40 -0800

    loop through successful fits

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -14,18 +14,22 @@ $(document).ready(function() {
       },
 
       success: function(response) {
-        // for(i=0; i < response.data.length; i++) {
-        //   console.log(response.data[i].profile.first_name)
-        // }
-        $('.first').text(" " +`${response.data[0].profile.first_name}`);
+        for(let i=0; i < response.data.length; i++) {
+          setTimeout(function() { console.log(response.data[i].profile.first_name); },1000);
+          let first = response.data[i].profile.first_name;
+
+
+
+        $('.first').text(first);
         $('.last').text(" " + `${response.data[0].profile.last_name}`);
         $('.address').text(" " + `${response.data[0].practices[0].visit_address.street}`);
         $('.phone').text(" " + `${response.data[0].practices[0].phones[0].number}`);
         $('.website').text(" " + `${response.data[0].practices[0].website}`); // issue
         $('.patient').text(" " + `${response.data[0].practices[0].accepts_new_patients}`);
+      }
       },
       error: function() {
-        $('.error').text("")
+        $('.errors').text("There was an error with the request.")
       }
     });
   });
