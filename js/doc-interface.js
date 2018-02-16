ce4cf6cca59d6984f42fb8c1a8ac21f1844569ce
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-16 15:20:55 -0800

    switch back to one js file, forgot to rebuild before last commit

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -6,5 +6,27 @@ $(document).ready(function() {
     event.preventDefault();
     let symptom = $('#symptom').val();
     $('#symptom').val("");
+    $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=OR&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=${apiKey}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+
+      success: function(response) {
+        // for(i=0; i < response.data.length; i++) {
+        //   console.log(response.data[i].profile.first_name)
+        // }
+        $('.first').text(" " +`${response.data[0].profile.first_name}`);
+        $('.last').text(" " + `${response.data[0].profile.last_name}`);
+        $('.address').text(" " + `${response.data[0].practices[0].visit_address.street}`);
+        $('.phone').text(" " + `${response.data[0].practices[0].phones[0].number}`);
+        $('.website').text(" " + `${response.data[0].practices[0].website}`); // issue
+        $('.patient').text(" " + `${response.data[0].practices[0].accepts_new_patients}`);
+      },
+      error: function() {
+        $('.error').text("")
+      }
+    });
   });
 });
--- js/doc.js
+++ js/doc.js
@@ -1,23 +1,5 @@
-function medCall(symptom, name) {
+function medCall() {
 
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=OR&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=${apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-
-      success: function(response) {
-        $('.first').text(" " +`${response.data[0].profile.first_name}`);
-        $('.last').text(" " + `${response.data[0].profile.last_name}`);
-        $('.address').text(" " + `${response.data[0].practices[0].visit_address.street}`);
-        $('.phone').text(" " + `${response.data[0].practices[0].phones[0].number}`);
-        $('.website').text(" " + `${response.data[0].practices[0].website}`); // issue
-        $('.patient').text(" " + `${response.data[0].practices[0].accepts_new_patients}`);
-      },
-      error: function() {
-        $('.error').text("")
-      }
-    });
+  
 
   }
