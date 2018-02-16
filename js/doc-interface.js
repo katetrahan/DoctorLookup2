b59773c66e721c445afc7d2675cb02e3837085c2
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-16 10:06:14 -0800

    add html and success and error function

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -3,5 +3,21 @@ import { Doc } from './../js/doc.js';
 $(document).ready(function() {
   $('#sym-form').submit(function(event){
     event.preventDefault();
+    let symptom = $('#symptom').val();
+    $('#symptom').val("");
+    $.ajax({
+      url: ``
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showFirst').text(`${}`);
+        $('.showLast').text(`${}`);
+      },
+      error: function() {
+        $('.error').text("")
+      }
+    });
   });
 });
