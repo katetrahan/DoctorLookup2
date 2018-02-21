5ba794cdc4e20f01f0c807be09172f27cf721278
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-02-20 17:12:35 -0800

    edit functionality, erase extra comments

--- js/doc.js
+++ js/doc.js
@@ -10,15 +10,23 @@ export function medCall(symptom) {
       if(response.data.length === 0) {
         $('.errors').text("there are no doctors listed in your area for this symptom");
       }
+
       for(let i=0; i < response.data.length; i++) {
         let first = response.data[i].profile.first_name;
         let last = response.data[i].profile.last_name;
         let address = response.data[i].practices[0].visit_address.street;
         let phone = response.data[i].practices[0].phones[0].number;
         let web = response.data[i].practices[0].website;
+        let nw = "this doctor does not have a website";
         let patient = response.data[i].practices[0].accepts_new_patients;
-      $('.doctor').append("<ul>" + "<li>First:" + " " + first + "</li>" + "<li>Last: " + " " + last + "</li>" + "<li>Address: " + " " + address + "</li>" + "<li>Phone:" + " " + phone + "</li>" +
-       "<li>Accepts new patients:" + " " + patient + "<li>Website:" + " " + web + "</ul>");
+
+        if(response.data[i].practices[0].website === undefined) {
+         $('.doctor').append("<ul>" + "<li>First:" + " " + first + "</li>" + "<li>Last: " + " " + last + "</li>" + "<li>Address: " + " " + address + "</li>" + "<li>Phone:" + " " + phone + "</li>" +
+          "<li>Accepts new patients:" + " " + patient + "</li>" + "<li>Website: " + " " + nw + "</ul>");
+        } else {
+        $('.doctor').append("<ul>" + "<li>First:" + " " + first + "</li>" + "<li>Last: " + " " + last + "</li>" + "<li>Address: " + " " + address + "</li>" + "<li>Phone:" + " " + phone + "</li>" +
+         "<li>Accepts new patients:" + " " + patient + "</li>" + "<li>Website: " + " " + web + "</ul>");
+      }
      }
     },
     error: function() {
